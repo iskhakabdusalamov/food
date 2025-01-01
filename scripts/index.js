be0b0803071c8e34b463969ca6e1310deaf8bb50
Iskhak <iskhakabdusalamovg@gmail.com>
2025-01-01 21:00:40 +0500

    timer

--- scripts/index.js
+++ scripts/index.js
@@ -18,15 +18,15 @@ window.addEventListener("DOMContentLoaded", () => {
 
   // tabs
   const tabContent = document.querySelectorAll(".tabContent");
-  const tabItems = document.querySelector(".tabHeaderItems");
-  const tabItem = document.querySelectorAll(".tabHeaderItem");
+  const tabParent = document.querySelector(".tabHeaderItems");
+  const tabs = document.querySelectorAll(".tabHeaderItem");
 
   const hideTabContent = () => {
     tabContent.forEach((item) => {
       item.classList.add("hide");
       item.classList.remove("show", "fade");
     });
-    tabItem.forEach((item) => {
+    tabs.forEach((item) => {
       item.classList.remove("tabHeaderItemActive");
     });
   };
@@ -36,15 +36,15 @@ window.addEventListener("DOMContentLoaded", () => {
     tabContent[index].classList.add("show", "fade");
     tabContent[index].classList.remove("hide");
 
-    tabItem[index].classList.add("tabHeaderItemActive");
+    tabs[index].classList.add("tabHeaderItemActive");
   };
   showTabContent();
 
-  tabItems.addEventListener("click", (e) => {
+  tabParent.addEventListener("click", (e) => {
     const target = e.target;
 
     if (target && target.classList.contains("tabHeaderItem")) {
-      tabItem.forEach((item, index) => {
+      tabs.forEach((item, index) => {
         if (target == item) {
           hideTabContent();
           showTabContent(index);
@@ -52,4 +52,41 @@ window.addEventListener("DOMContentLoaded", () => {
       });
     }
   });
+
+  // timer
+  const getTimeRemaining = () => {
+    const total = Date.parse("2025-05-20") - Date.now();
+    if (total > 0) {
+      const days = Math.floor(total / (1000 * 60 * 60 * 24));
+      const hours = Math.floor((total / (1000 * 60 * 60)) % 24);
+      const minutes = Math.floor((total / (1000 * 60)) % 60);
+      const seconds = Math.floor((total / 1000) % 60);
+      return { days, hours, minutes, seconds, total };
+    } else {
+      return { days: 0, hours: 0, minutes: 0, seconds: 0 };
+    }
+  };
+
+  const getZero = (number) => {
+    if (number >= 0 && number <= 9) return `0${number}`;
+    return number;
+  };
+
+  const setClock = () => {
+    const updateClock = () => {
+      const { days, hours, minutes, seconds, total } = getTimeRemaining();
+      document.querySelector("#days").textContent = getZero(days);
+      document.querySelector("#hours").textContent = getZero(hours);
+      document.querySelector("#minutes").textContent = getZero(minutes);
+      document.querySelector("#seconds").textContent = getZero(seconds);
+
+      if (total < 1) {
+        clearInterval(timeInterval);
+      }
+    };
+
+    const timeInterval = setInterval(updateClock, 1000);
+    updateClock();
+  };
+  setClock();
 });
